dcf92f6a1139fe83840b81b9ee6121626ba38067
Leonidas <leonidas.drawanz@gmail.com>
2022-03-23 18:10:46 -0300

    testes 1, 2 e 3 do pokemon details

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+// test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('', () => {
+  beforeEach(() => {
+
+  });
+
+  it('Teste se é renderizado um card com o nome de determinado pokémon', () => {
+    const { history } = renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    history.push({ pathname: `/pokemons/${pokemons[0].id}` });
+
+    const pikachuDetails = screen.getByRole('heading', { name: /Pikachu Details/i });
+    const summary = screen.getByRole('heading', { name: /Summary/i });
+    const resume = screen.getByText(pokemons[0].summary);
+
+    expect(moreDetails).not.toBeInTheDocument();
+    expect(pikachuDetails).toBeInTheDocument();
+    expect(summary).toBeInTheDocument();
+    expect(resume).toBeInTheDocument();
+  });
+
+  it('Teste se existe na página mapas contendo as localizações do pokémon', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push({ pathname: `/pokemons/${pokemons[0].id}` });
+
+    const locations = screen.getByRole('heading', { name: /Game Locations of Pikachu/i });
+    const locationsImg = screen.getAllByRole('img', { name: /Pikachu location/i });
+    const textFirstLocation = screen.getByText(/kanto viridian forest/i);
+    const textSecondLocation = screen.getByText(/kanto power plant/i);
+
+    expect(locations).toBeInTheDocument();
+    expect(locationsImg).toHaveLength(2);
+    expect(locationsImg[0].src).toBe(pokemons[0].foundAt[0].map);
+    expect(locationsImg[0].alt).toBe('Pikachu location');
+    expect(locationsImg[1].src).toBe(pokemons[0].foundAt[1].map);
+    expect(locationsImg[1].alt).toBe('Pikachu location');
+    expect(textFirstLocation).toBeInTheDocument();
+    expect(textSecondLocation).toBeInTheDocument();
+  });
+
+  it('Teste se o usuário pode favoritar um pokémon através da página de detalhes', () => {
+    
+  });
+});
