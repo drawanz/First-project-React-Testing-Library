0519ee1927b72cce9816e58634eb7e7fb2153e1c
Leonidas <leonidas.drawanz@gmail.com>
2022-03-23 21:17:44 -0300

    refatorando o requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,9 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 describe('Verifica o componente <FavoritePokemons.js />', () => {
   it('Verifica se o componente está vazio', () => {
@@ -12,5 +14,25 @@ describe('Verifica o componente <FavoritePokemons.js />', () => {
 
     expect(favorites).toBeInTheDocument();
     expect(noFavorites).toBeInTheDocument();
+  });
+
+  it('Verifica se o componente possui pokemons favoritados', () => {
+    renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+
+    const checkbox = screen.getByText(/pokémon favoritado\?/i);
+    userEvent.click(checkbox);
+
+    const favorites = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favorites);
+
+    const favoritesHeading = screen
+      .getByRole('heading', { name: /Favorite pokémons/i });
+    const pikachu = screen.getByText(/pikachu/i);
+
+    expect(favoritesHeading).toBeInTheDocument();
+    expect(pikachu).toBeInTheDocument();
   });
 });
